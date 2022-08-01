14de0763e9aacf73dc435fa8260c9825abe07cca
scirelli <scirelli+git@gmail.com>
2022-08-01 15:52:00 -0400

    Adding log factory

---
+++ src/modules/logFactory.js
@@ -0,0 +1,23 @@
+const debug = require('debug');
+const LOGGERS = ['assert', 'debug', 'error', 'exception', 'info', 'log', 'trace', 'warn'];
+
+function enable(loggers=LOGGERS) {
+    if(!loggers || !loggers.length) return;
+    if(!Array.isArray(loggers)) {loggers = [loggers];}
+
+    loggers.forEach(l=> {
+        if(this[l]) this[l].enabled = true;
+    });
+}
+
+function createLogger(name) {
+    return LOGGERS.reduce((a, m)=>{
+        a[m] = debug(`${m.toUpperCase()}:${name}`);
+        return a;
+    }, {enable: enable});
+}
+
+module.exports = {
+    createLogger: createLogger,
+    enable: enable
+};
