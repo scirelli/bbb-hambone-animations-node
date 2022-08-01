d34f59c759e26db102a4287127870e985e8b6d13
scirelli <scirelli+git@gmail.com>
2022-08-01 17:15:47 -0400

    Turn on a pixel

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
-const fs = require('fs/promises'),
+const fs = require('fs'),
     logFactory = require('./modules/logFactory.js');
 
 const log = logFactory.create('BBB');
 const DEV_FILE = '/dev/rpmsg_pru30';
 
 (async ()=>{
-    try {
-        const content = '0 0 0 0\n-1 0 0 0\n';
-        await fs.writeFile(DEV_FILE, content, {flag: 'a'});
-    } catch (err) {
-        log(err);
-    }
+    const commands = ['0 255 0 0\n', '-1 0 0 0\n'];
+    commands.forEach(c => {
+        fs.writeFile(DEV_FILE, commands[i], {flag: 'a'}, (err)=>{
+            log.error(err);
+        });
+    });
 })();
