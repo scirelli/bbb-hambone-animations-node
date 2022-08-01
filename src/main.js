afe6ab2ae2c903269bdaea61428827e2a4b4c504
scirelli <scirelli+git@gmail.com>
2022-08-01 19:25:54 -0400

    Attempt writing streams

--- src/main.js
+++ src/main.js
@@ -4,9 +4,15 @@ const fs = require('fs'),
 
 const log = logFactory.create('BBB');
 const DEV_FILE = '/dev/rpmsg_pru30';
+const commands = ['0 255 0 0', '-1 0 0 0'];
+const file = fs.createWriteStream(DEV_FILE);
+file.write(commands[0]);
+file.end(commands[1]);
 
-commands.forEach(c => {
-    fs.writeFile(DEV_FILE, c, {flag: 'a'}, (err)=>{
-        log.error(err);
-    });
-});
+// commands.forEach(c => {
+//     fs.writeFile(DEV_FILE, c, {flag: 'a'}, (err)=>{
+//         log.error(err);
+//     });
+// });
+// fs.open(DEV_FILE, 'w', (err,fd)=>{
+//});
