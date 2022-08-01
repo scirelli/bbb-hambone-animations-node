a688d4ceebfd820167ff260396ed91244e57a0d9
scirelli <scirelli+git@gmail.com>
2022-08-01 16:17:18 -0400

    Building up a stub

---
+++ src/main.js
@@ -0,0 +1,14 @@
+const fs = require('fs/promises'),
+    logFactory = require('./modules/logFactory.js');
+
+const log = logFactory.create('BBB');
+const DEV_FILE = '/dev/rpmsg_pru30';
+
+(async ()=>{
+    try {
+        const content = '0 0 0 0\n-1 0 0 0\n';
+        await fs.writeFile(DEV_FILE, content, {flag: 'a'});
+    } catch (err) {
+        log(err);
+    }
+})();
