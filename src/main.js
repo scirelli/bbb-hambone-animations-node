618bb3b493f8adf9c5f9a9c59fb4be8ec1080e31
scirelli <scirelli+git@gmail.com>
2022-08-01 22:41:13 -0400

    testing

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 const fs = require('fs'),
-    logFactory = require('./modules/logFactory.js');
-
-import { setTimeout } from 'timers/promises';
+    logFactory = require('./modules/logFactory.js'),
+    {setTimeout} =  require('timers/promises');
 
 
 const log = logFactory.create('BBB');
@@ -10,7 +9,7 @@ const DEV_FILE = '/dev/rpmsg_pru30';
 const commands = ['0 255 0 0', '-1 0 0 0', '1 0 255 0', '-1 0 0 0'];
 const file = fs.createWriteStream(DEV_FILE);
 
-['0 255 0 0', '-1 0 0 0', '1 0 255 0', '-1 0 0 0'].forEach(c=>{
+['0 255 0 0', '-1 0 0 0', '1 0 255 0', '-1 0 0 0', '0 0 0 0', '1 0 0 0'].forEach(c=>{
     file.write(c);
 });
 
